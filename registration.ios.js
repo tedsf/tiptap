531447cebd6593f91f2951de2b2c2a7ccc7b2051
Chris Otto <email": null>
2016-07-25 17:28:09 -0700

    Refactor for formatting, style

--- registration.ios.js
+++ registration.ios.js
@@ -17,70 +17,70 @@ import {
 } from 'react-native'
 
 class Registration extends Component {
-    navigate(routeName) {
-      this.props.navigator.push({
-        name: routeName
-      });
-    }
+  constructor(props) {
+    super(props);
+    this.state = {first_name: '', last_name: '', photo_url: '', payment_url: ''};
+  }
 
-    constructor(props) {
-      super(props);
-      this.state = {first_name: '', last_name: '', photo_url: '', payment_url: ''};
-    }
-
-    _onPressButtonPOST() {
-      console.log(this.state)
-      fetch("https://tiptap-api.herokuapp.com/tippees", {
-        method: "POST",
-        headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      }, body: JSON.stringify({tippee: this.state})})
-      .then((response) => response.json())
-      .done();
-    }
+  navigate(routeName) {
+    this.props.navigator.push({
+      name: routeName
+    });
+  }
 
   render() {
     return (
       <View>
-      <NavigationBar
-          title={{ title:  'TipTap!' , tintColor:  'black' , }}
-          leftButton={{ title: 'Back', tintColor: 'black', handler: this.navigate.bind(this, "main")} }
-          style={{ backgroundColor:  "#D3D3D3" , }}
-          statusBar={{ tintColor:  "white" , }}
-      />
-
-      <View style={{padding: 50}}>
-        <TextInput
-          style={{fontSize: 20, height: 50, borderColor: 'gray', borderWidth: 2, padding: 3, marginTop: 1}}
-          placeholder=" First Name"
-          onChangeText={(text) => this.setState({first_name: text})}
+        <NavigationBar
+            title={{ title:  'TipTap!' , tintColor:  'black' , }}
+            leftButton={{ title: 'Back', tintColor: 'black', handler: this.navigate.bind(this, "main")} }
+            style={{ backgroundColor:  "#D3D3D3" , }}
+            statusBar={{ tintColor:  "white" , }}
         />
-        <TextInput
-          style={{fontSize: 20, height: 50, borderColor: 'gray', borderWidth: 2, padding: 3, marginTop: 1}}
-          placeholder=" Last Name"
-          onChangeText={(text) => this.setState({last_name: text})}
-         />
-        <TextInput
-           style={{fontSize: 20, height: 50, borderColor: 'gray', borderWidth: 2, padding: 3, marginTop: 1}}
-           placeholder=" Photo URL"
-           onChangeText={(text) => this.setState({photo_url: text})}
+
+        <View style={{padding: 50}}>
+          <TextInput
+            style={{fontSize: 20, height: 50, borderColor: 'gray', borderWidth: 2, padding: 3, marginTop: 1}}
+            placeholder=" First Name"
+            onChangeText={(text) => this.setState({first_name: text})}
           />
-        <TextInput
-            style={{fontSize: 20, height: 50, borderColor: 'gray', borderWidth: 2, padding: 3, marginTop: 1, marginBottom: 1}}
-            placeholder=" Payment URL"
-            onChangeText={(text) => this.setState({payment_url: text})}
-        />
+          <TextInput
+            style={{fontSize: 20, height: 50, borderColor: 'gray', borderWidth: 2, padding: 3, marginTop: 1}}
+            placeholder=" Last Name"
+            onChangeText={(text) => this.setState({last_name: text})}
+           />
+          <TextInput
+             style={{fontSize: 20, height: 50, borderColor: 'gray', borderWidth: 2, padding: 3, marginTop: 1}}
+             placeholder=" Photo URL"
+             onChangeText={(text) => this.setState({photo_url: text})}
+            />
+          <TextInput
+              style={{fontSize: 20, height: 50, borderColor: 'gray', borderWidth: 2, padding: 3, marginTop: 1, marginBottom: 1}}
+              placeholder=" Payment URL"
+              onChangeText={(text) => this.setState({payment_url: text})}
+          />
 
-        <Button large success block
-          onPress={this._onPressButtonPOST()}>
-          Submit Registration
-          {/*onPress={this.navigate.bind(this, "main")}*/}
-        </Button>
+          <Button large success block
+            onPress={this.registerTippee()}>
+            Submit Registration
+            {/*onPress={this.navigate.bind(this, "main")}*/}
+          </Button>
 
-      </View>
+        </View>
       </View>
     );
+  }
+
+  registerTippee() {
+    console.log(this.state)
+    fetch("https://tiptap-api.herokuapp.com/tippees", {
+      method: "POST",
+      headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }, body: JSON.stringify({tippee: this.state})})
+    .then((response) => response.json())
+    .done();
   }
 }
 
