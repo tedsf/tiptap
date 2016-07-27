408faf5944a17cc73daa3779975ca1cf37a76679
Ben Flores <benjaminhyw@gmail.com>
2016-07-27 16:49:11 -0700

    Add registration validation

--- registration.ios.js
+++ registration.ios.js
@@ -54,73 +54,77 @@ class Registration extends Component {
         />
 
         <ScrollView style={{padding: 50}} ref='scrollView'>
-        <TextInput
-         ref='FirstName'
-         style={{fontSize: 20, height: 50, borderColor: 'gray', borderWidth: 2, padding: 3, marginTop: 1}}
-         placeholder=" First Name"
-         onChangeText={(text) => this.setState({first_name: text})}
-         onSubmitEditing={(event) => {
-           this.refs.LastName.focus();
-         }}
-       />
-       <TextInput
-         ref='LastName'
-         style={{fontSize: 20, height: 50, borderColor: 'gray', borderWidth: 2, padding: 3, marginTop: 1}}
-         placeholder=" Last Name"
-         onChangeText={(text) => this.setState({last_name: text})}
-         onSubmitEditing={(event) => {
-           this.refs.PhotoUrl.focus();
-         }}
-         onFocus={this.inputFocused.bind(this, 'LastName')}
-        />
-       <TextInput
-         ref='PhotoUrl'
-         style={{fontSize: 20, height: 50, borderColor: 'gray', borderWidth: 2, padding: 3, marginTop: 1}}
-         placeholder=" Photo URL"
-         onChangeText={(text) => this.setState({photo_url: text})}
-         onSubmitEditing={(event) => {
-           this.refs.PaymentUrl.focus();
-         }}
-         onFocus={this.inputFocused.bind(this, 'PhotoUrl')}
-         />
-       <TextInput
-         ref='PaymentUrl'
-         style={{fontSize: 20, height: 50, borderColor: 'gray', borderWidth: 2, padding: 3, marginTop: 1, marginBottom: 1}}
-         placeholder=" Payment URL"
-         onChangeText={(text) => this.setState({payment_url: text})}
-         onFocus={this.inputFocused.bind(this, 'PaymentUrl')}
-       />
+          <TextInput
+             ref='FirstName'
+             style={{fontSize: 20, height: 50, borderColor: 'gray', borderWidth: 2, padding: 3, marginTop: 1}}
+             placeholder=" First Name"
+             onChangeText={(text) => this.setState({first_name: text})}
+             onSubmitEditing={(event) => {
+               this.refs.LastName.focus();
+             }}
+          />
+          <TextInput
+           ref='LastName'
+           style={{fontSize: 20, height: 50, borderColor: 'gray', borderWidth: 2, padding: 3, marginTop: 1}}
+           placeholder=" Last Name"
+           onChangeText={(text) => this.setState({last_name: text})}
+           onSubmitEditing={(event) => {
+             this.refs.PhotoUrl.focus();
+           }}
+           onFocus={this.inputFocused.bind(this, 'LastName')}
+          />
+          <TextInput
+           ref='PhotoUrl'
+           style={{fontSize: 20, height: 50, borderColor: 'gray', borderWidth: 2, padding: 3, marginTop: 1}}
+           placeholder=" Photo URL"
+           onChangeText={(text) => this.setState({photo_url: text})}
+           onSubmitEditing={(event) => {
+             this.refs.PaymentUrl.focus();
+           }}
+           onFocus={this.inputFocused.bind(this, 'PhotoUrl')}
+          />
+          <TextInput
+           ref='PaymentUrl'
+           style={{fontSize: 20, height: 50, borderColor: 'gray', borderWidth: 2, padding: 3, marginTop: 1, marginBottom: 1}}
+           placeholder=" Payment URL"
+           onChangeText={(text) => this.setState({payment_url: text})}
+           onFocus={this.inputFocused.bind(this, 'PaymentUrl')}
+          />
 
-          <Button large success block
-            onPress={this.registerTippee.bind(this)}>
+          <Button large success block onPress={this.registerTippee.bind(this)}>
             Submit Registration
           </Button>
-
         </ScrollView>
       </View>
     );
   }
 
   registerTippee() {
-    var that = this
-    fetch("https://tiptap-api.herokuapp.com/tippees", {
-      method: "POST",
-      headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    }, body: JSON.stringify({tippee: that.state})})
-    .then((response) => response.json())
-    .then((responseJson) => {
-      AsyncStorage.setItem(
-        'beacons',
-        JSON.stringify(responseJson.beacons),
-        () => that.navigate("main")
-      )
-    })
-    .done();
+    if (this.state.first_name && this.state.last_name && this.state.payment_url)
+      {
+      var that = this
+      fetch("https://tiptap-api.herokuapp.com/tippees", {
+        method: "POST",
+        headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }, body: JSON.stringify({tippee: that.state})})
+      .then((response) => response.json())
+      .then((responseJson) => {
+        AsyncStorage.setItem(
+          'beacons',
+          JSON.stringify(responseJson.beacons),
+          () => that.navigate("main")
+        )
+      })
+      .done();
+    } else {
+      AlertIOS.alert(
+        "Missing required fields!",
+        "- Team TipTap")
+    }
   }
 }
-
 
 const styles = StyleSheet.create({
   container: {
