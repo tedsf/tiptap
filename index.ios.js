67dc64bb7f2f3f52cf837321dd82c6f1059e1d06
Ted Day-Fratto <tdfratto@gmail.com>
2016-07-28 11:52:26 -0700

    Refactor index file

--- index.ios.js
+++ index.ios.js
@@ -1,23 +1,9 @@
 import React, { Component } from 'react';
-import NavigationBar from 'react-native-navbar'
-import Button from 'react-native-button';
-import Beacons from 'react-native-ibeacon';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  SegmentedControlIOS,
-  Image,
-  Switch,
-  DatePickerIOS,
-  Navigator,
-  AsyncStorage,
-} from 'react-native'
+import { AppRegistry, Navigator } from 'react-native';
 
-import Main from './main'
-import Registration from './registration'
-import Active from './active'
+import Main from './main';
+import Registration from './registration';
+import Active from './active';
 
 class TipTap extends Component {
   render() {
@@ -33,32 +19,13 @@ class TipTap extends Component {
     if(route.name === 'main') {
       return <Main navigator={navigator} {...route.passProps} />
     }
-    if(route.name === 'active') {
+    else if(route.name === 'active') {
       return <Active navigator={navigator} {...route.passProps} />
     }
-    if(route.name === 'registration') {
+    else if(route.name === 'registration') {
       return <Registration navigator={navigator} {...route.passProps} />
     }
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('Project', () => TipTap);
